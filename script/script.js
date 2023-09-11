7979fcbbd879f9ce8ce984f590cedffd3649f036
Iury96 <iury.freitas961@gmail.com>
2023-09-11 16:53:50 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('botao')
+
+
+function pegarCep() {
+    const cep = document.getElementById('cep')
+
+}
+function clicou() {
+    console.log('foi');
+}
+botao.addEventListener("click", clicou());
