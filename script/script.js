c6740d46d55765d3b52686cd1fb5e7e461c504ec
Iury96 <iury.freitas961@gmail.com>
2023-09-14 14:31:47 -0300

    script

--- script/script.js
+++ script/script.js
@@ -1,16 +1,19 @@
 
-// URL da API
-const apiUrl = `https://viacep.com.br/ws/01001000/json/`;
+function fetchCEPData(cep) {
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
 
-// Requisição usando fetch
-fetch(apiUrl)
-  .then(response => {
-  
-    return response.json();
-  })
+  // Retorna a promessa gerada pelo fetch
+  return fetch(apiUrl)
+    .then(response => {
+      return response.json();
+    });
+}
 
+// Uso da função:
+fetchCEPData('22450270')
   .then(data => {
-    
+    console.log(data.logradouro);
     console.log(data.localidade);
-  })
- 
+    console.log(data.bairro);
+    console.log(data.ddd);
+  });
