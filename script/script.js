cff1f43ecf5f7fe7b672b9ad886a32b5131f4abd
Iury96 <iury.freitas961@gmail.com>
2023-09-14 23:44:57 -0300

    taindokk

--- script/script.js
+++ script/script.js
@@ -1,19 +1,43 @@
+const botao = document.querySelector('#botao');
 
 function fetchCEPData(cep) {
   const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
 
-  // Retorna a promessa gerada pelo fetch
   return fetch(apiUrl)
     .then(response => {
       return response.json();
     });
 }
 
-// Uso da função:
-fetchCEPData('22450270')
-  .then(data => {
-    console.log(data.logradouro);
-    console.log(data.localidade);
-    console.log(data.bairro);
-    console.log(data.ddd);
-  });
+
+function consultarEndereco(evento) {
+  evento.preventDefault();
+  const cep = document.querySelector('#cep').value;
+
+  if (cep.length !== 8) {
+    alert('CEP incorreto.');
+    return;
+  }
+
+  fetchCEPData(cep)
+    .then(data => mostrarResultado(data))
+    .catch(error => {
+      console.error(error);
+      alert('Erro ao consultar o CEP.');
+    });
+}
+
+function mostrarResultado(data) {
+  const resultado = document.querySelector('#resultado')
+  if (data.erro) {
+    resultado.innerHTML = "Não foi possível localizar!"
+  } else {
+    resultado.innerHTML = `
+  <p>Endereço: ${data.logradouro}</p>
+  <p>Endereço: ${data.ddd}</p>
+  
+  `
+  }
+}
+
+botao.addEventListener('click', consultarEndereco);
